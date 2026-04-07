eb7ff5385de85e33fa033e917a49eb4c2c0f3770
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-04-07 12:16:57 +0530

    Added eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    ignores: ["dist/**", "node_modules/**", "jest.config.ts"],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+
+      "no-console": "warn",
+      "prefer-const": "error",
+      "eqeqeq": ["error", "always"],
+    },
+  },
+];
